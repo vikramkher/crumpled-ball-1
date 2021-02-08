f817b588000a0f8327f5ff803640ceb9cbe7645c
vikramkher <74855089+vikramkher@users.noreply.github.com>
2021-02-08 14:22:21 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,19 @@
+class Paper {
+    constructor(x, y, radius){
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.2,
+            'isStatic':false
+        }
+        this.radius = radius
+        this.body = Bodies.circle(x, y, radius, options);
+        World.add(world, this.body);
+
+    }
+    display(){
+        fill("red");
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+    }
+}
